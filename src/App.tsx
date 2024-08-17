524861cd0a677004ce25e785d9143b0b9346abb8
Sarah Tulimat <sarahjtul@gmail.com>
2024-08-17 15:42:31 +0300

    rout

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,8 @@
-
-import { createBrowserRouter , RouterProvider } from 'react-router-dom';
-import Sarah from './pages/Sarah';
-import Mourad from './pages/Mourad';
+import Sarah from "./pages/Sarah";
 
 
-let router = createBrowserRouter([
-  {
-    path: "/sarah",
-    loader: () => ({ message: "Hello Data Router!" }),
-    Component() {
-      return <Sarah />;
-    },
-  },
-  {
-    path: "/mourad",
-    loader: () => ({ message: "Hello Data Router!" }),
-    Component() {
-      return <Mourad/>;
-    },
-  },
-]);
 
 export default function App() {
-  return <RouterProvider router={router}  />;
+  return <Sarah />
 }
 
