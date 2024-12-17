221342032b70fa28402c242c397c6bd2956dc4c4
sarahtul21 <90712937+sarahtul21@users.noreply.github.com>
2024-12-17 14:33:32 +0300

    Update Sarah.jsx

--- src/pages/Sarah.jsx
+++ src/pages/Sarah.jsx
@@ -1,3 +1,4 @@
+import tadawol from '../img/tadawol.png'
 import alfajr from "../img/alfajr.png"
 import monday from "../img/monday.png"
 import tryon from "../img/tryon.png"
@@ -7,6 +8,7 @@ import assist from "../img/assist.png"
 import ordit from "../img/ordit.png"
 import { Button, Modal } from "flowbite-react";
 import { useState } from "react";
+import alshamma from '../img/alshamma.png'
 
 
 
@@ -15,23 +17,23 @@ function Sarah() {
   const [openModalFrames, setOpenModalFrames] = useState(false);
   const [openModalTablet, setOpenModalTablet] = useState(false);
   const [openModalMonday, setOpenModalMonday] = useState(false);
-
+  const [openModalTadawol, setOpenModalTadawol] = useState(false);
   return (
     <>
 
     <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-      <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+      <a href="" className="flex items-center space-x-3 rtl:space-x-reverse">
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Portfoilo</span>
       </a>
-      <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse md:hidden">
+      {/* <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse md:hidden">
         <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white hover:text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200  dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
             <span className="sr-only">Open main menu</span>
             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
                 <path className="stroke-black"  strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
             </svg>
         </button>
-      </div>
+      </div> */}
       <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
         <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
           <li>
@@ -65,6 +67,9 @@ function Sarah() {
     
       <h2 className="my-8 text-2xl tracking-tight leading-none text-black md:text-3xl lg:text-4xl text-center ">Projects</h2>
       <div className="flex flex-wrap flex-row justify-evenly py-4 gap-4">
+
+
+
         <div className="max-w-sm bg-white p-2 grid place-items-center text-center border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
           <img className="rounded-t-lg" src={alfajr} alt="" />
           <div className="p-5">
@@ -107,6 +112,49 @@ function Sarah() {
               </a>
           </div>
         </div>
+
+        <div className="max-w-sm bg-white p-2 grid place-items-center text-center   border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+          <img className="rounded-t-lg" src={alshamma} alt="" />
+          <div className="p-5">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Alshamma Company</h5>
+              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Laravel , bootstrap</p>
+              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">website for booking shipping and cleaning appointments</p>
+              <a  href="https://alshamaa.de/" target="_blank" rel="noopener" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                  website link
+                  <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                  </svg>
+              </a>
+          </div>
+        </div>
+
+        
+
+        <div className="max-w-sm bg-white p-2 grid place-items-center text-center   border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+          <img className="rounded-t-lg" src={tadawol} alt="" />
+          <div className="p-5">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">INZO Dashboard</h5>
+              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">React , chart js , tailwind , daisyUI</p>
+              
+              <Button onClick={() => setOpenModalTadawol(true)} className=" m-auto block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Details</Button>
+              <Modal show={openModalTadawol} onClose={() => setOpenModalTadawol(false)}>
+                <Modal.Header>A Trading Company Dashboard</Modal.Header>
+                <Modal.Body>
+                  <div className="space-y-6">
+                    <iframe className='m-auto max-w-[90%]' width="560" height="315" src="https://youtu.be/qzZXTHIjXEU?si=-uTF9epUba8uNkGo" title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>                        
+                    <p className='px-3'>
+                    Design a financial table with filter , search, onscroll loading mor rows ... ,  charts for employees activities and working hours
+                  </p>
+                  </div>
+                </Modal.Body>
+                <Modal.Footer>
+                  <Button color="gray" className='m-auto mb-3' onClick={() => setOpenModalTadawol(false)}>
+                    Close
+                  </Button>
+                </Modal.Footer>
+              </Modal>
+          </div>
+        </div>
     
         <div className="max-w-sm bg-white p-2 grid place-items-center text-center   border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
           <img className="rounded-t-lg" src={tablet} alt="" />
@@ -119,14 +167,14 @@ function Sarah() {
                 <Modal.Header>A real estate website</Modal.Header>
                 <Modal.Body>
                   <div className="space-y-6">
-                    <iframe className='m-auto' width="560" height="315" src="https://www.youtube.com/embed/J7LlI1lrB1I?si=f0n0O5FzJaEve3Hz" title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>                        
-                    <p>
+                    <iframe className='m-auto max-w-[90%]' width="560" height="315" src="https://www.youtube.com/embed/J7LlI1lrB1I?si=f0n0O5FzJaEve3Hz" title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>                        
+                    <p className='px-3'>
                     Designed a scroll section with a fixed on-scroll video player and a movable text content part
                   </p>
                   </div>
                 </Modal.Body>
                 <Modal.Footer>
-                  <Button color="gray" onClick={() => setOpenModalTablet(false)}>
+                  <Button color="gray" className='m-auto mb-3' onClick={() => setOpenModalTablet(false)}>
                     Close
                   </Button>
                 </Modal.Footer>
@@ -145,14 +193,14 @@ function Sarah() {
                 <Modal.Header>Real Estate website</Modal.Header>
                 <Modal.Body>
                   <div className="space-y-6">
-                    <iframe className='m-auto' width="560" height="315" src="https://www.youtube.com/embed/fln1qYo6nX0?si=SX3d7cHy5w1dxi3R" title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
-                    <p>
+                    <iframe className='m-auto max-w-[90%]' width="560" height="315" src="https://www.youtube.com/embed/fln1qYo6nX0?si=SX3d7cHy5w1dxi3R" title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
+                    <p className='px-3'>
                       Looks like 3D,using animation frame & 119 images
                     </p>
                   </div>
                 </Modal.Body>
                 <Modal.Footer>
-                  <Button color="gray" onClick={() => setOpenModalFrames(false)}>
+                  <Button color="gray" className='m-auto mb-3' onClick={() => setOpenModalFrames(false)}>
                     Close
                   </Button>
                 </Modal.Footer>
@@ -171,8 +219,8 @@ function Sarah() {
                 <Modal.Header>website similar to 'Monday’</Modal.Header>
                 <Modal.Body>
                   <div className="space-y-6">
-                    <iframe className='m-auto' width="560" height="315" src="https://www.youtube.com/embed/2pA2krOU1RE?si=h4GNNJhrFnAi7clP" title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
-                    <p>
+                    <iframe className='m-auto max-w-[90%]' width="560" height="315" src="https://www.youtube.com/embed/2pA2krOU1RE?si=h4GNNJhrFnAi7clP" title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
+                    <p className='px-3'>
                       Designed a table with drag and
                       drop features, as well as the ability to edit, delete, and add
                       rows, columns & table. Each cell has its own unique style for
@@ -181,7 +229,7 @@ function Sarah() {
                   </div>
                 </Modal.Body>
                 <Modal.Footer>
-                  <Button color="gray" onClick={() => setOpenModalMonday(false)}>
+                  <Button color="gray" className='m-auto mb-3' onClick={() => setOpenModalMonday(false)}>
                     Close
                   </Button>
                 </Modal.Footer>
@@ -201,8 +249,8 @@ function Sarah() {
                 <Modal.Header> A makeup tryon dashboard</Modal.Header>
                 <Modal.Body>
                   <div className="space-y-6">
-                    <iframe className='m-auto' width="560" height="315" src="https://www.youtube.com/embed/uzkxGihgsxs?si=ipM81FV_xjp8dpck" title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
-                    <p>
+                    <iframe className='m-auto max-w-[90%]' width="560" height="315" src="https://www.youtube.com/embed/uzkxGihgsxs?si=ipM81FV_xjp8dpck" title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ></iframe>
+                    <p className='px-3'>
                       Designed a Form for adding and
                       editing the properties of items. Each field has its own unique
                       design with a distinctive way of displaying content, including
@@ -211,7 +259,7 @@ function Sarah() {
                   </div>
                 </Modal.Body>
                 <Modal.Footer>
-                  <Button color="gray" onClick={() => setOpenModalTryon(false)}>
+                  <Button color="gray" className='m-auto mb-3' onClick={() => setOpenModalTryon(false)}>
                     Close
                   </Button>
                 </Modal.Footer>
@@ -240,41 +288,40 @@ function Sarah() {
             </li>
             <li>
                 <a  target="_blank" rel="noopener" href='mailto:sarahjtul@gmail.com' className="hover:underline inline-block mt-[-13px]">
-    
-    {/* <!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools --> */}
-    <svg height="40px" width="40px" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" 
-       viewBox="0 0 64 64">
-    <style type="text/css">
-      {`.st0{fill:#77B3D4;}
-      .st1{opacity:0.2;}
-      .st2{fill:#231F20;}
-      .st3{fill:#FFFFFF;}`}
-    </style>
-    <g id="Layer_1">
-      <g>
-        <circle className="st0" cx="32" cy="32" r="32"/>
-      </g>
-      <g className="st1">
-        <path className="st2" d="M32,12c-12.1,0-22,9.9-22,22s9.9,22,22,22c3.5,0,7-0.8,10.1-2.4c1-0.5,1.4-1.7,0.9-2.7s-1.7-1.4-2.7-0.9
-          c-2.6,1.3-5.3,2-8.2,2c-9.9,0-18-8.1-18-18s8.1-18,18-18s18,8.1,18,18c0,3-0.8,6-2.2,8.6c-1.8-1.4-4.4-4.2-4.4-8.5v-8.8
-          c0-1.1-0.9-2-2-2s-2,0.9-2,2v0.1c-2-1.7-4.6-2.8-7.4-2.8c-6.3,0-11.4,5.1-11.4,11.4S25.7,45.4,32,45.4c3.7,0,7-1.8,9.1-4.6
-          c2.3,4.2,6.2,6.3,6.4,6.4c0.9,0.5,2,0.2,2.6-0.6c2.6-3.7,3.9-8,3.9-12.6C54,21.9,44.1,12,32,12z M32,41.4c-4.1,0-7.4-3.3-7.4-7.4
-          c0-4.1,3.3-7.4,7.4-7.4s7.4,3.3,7.4,7.4C39.4,38.1,36.1,41.4,32,41.4z"/>
-      </g>
-      <g>
-        <path className="st3" d="M32,54c-12.1,0-22-9.9-22-22s9.9-22,22-22s22,9.9,22,22c0,4.5-1.4,8.9-3.9,12.6c-0.6,0.8-1.7,1.1-2.6,0.6
-          c-0.3-0.2-8.1-4.2-8.1-13.1v-8.8c0-1.1,0.9-2,2-2c1.1,0,2,0.9,2,2v8.8c0,4.3,2.6,7.1,4.4,8.5C49.2,38,50,35,50,32
-          c0-9.9-8.1-18-18-18s-18,8.1-18,18s8.1,18,18,18c2.9,0,5.7-0.7,8.2-2c1-0.5,2.2-0.1,2.7,0.9c0.5,1,0.1,2.2-0.9,2.7
-          C39,53.2,35.5,54,32,54z"/>
-      </g>
-      <g>
-        <path className="st3" d="M32,24.6c4.1,0,7.4,3.3,7.4,7.4s-3.3,7.4-7.4,7.4s-7.4-3.3-7.4-7.4S27.9,24.6,32,24.6 M32,20.6
-          c-6.3,0-11.4,5.1-11.4,11.4S25.7,43.4,32,43.4S43.4,38.3,43.4,32S38.3,20.6,32,20.6L32,20.6z"/>
-      </g>
-    </g>
-    <g id="Layer_2">
-    </g>
-    </svg>
+                  {/* <!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools --> */}
+                  <svg height="40px" width="40px" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" 
+                    viewBox="0 0 64 64">
+                  <style type="text/css">
+                    {`.st0{fill:#77B3D4;}
+                    .st1{opacity:0.2;}
+                    .st2{fill:#231F20;}
+                    .st3{fill:#FFFFFF;}`}
+                  </style>
+                  <g id="Layer_1">
+                    <g>
+                      <circle className="st0" cx="32" cy="32" r="32"/>
+                    </g>
+                    <g className="st1">
+                      <path className="st2" d="M32,12c-12.1,0-22,9.9-22,22s9.9,22,22,22c3.5,0,7-0.8,10.1-2.4c1-0.5,1.4-1.7,0.9-2.7s-1.7-1.4-2.7-0.9
+                        c-2.6,1.3-5.3,2-8.2,2c-9.9,0-18-8.1-18-18s8.1-18,18-18s18,8.1,18,18c0,3-0.8,6-2.2,8.6c-1.8-1.4-4.4-4.2-4.4-8.5v-8.8
+                        c0-1.1-0.9-2-2-2s-2,0.9-2,2v0.1c-2-1.7-4.6-2.8-7.4-2.8c-6.3,0-11.4,5.1-11.4,11.4S25.7,45.4,32,45.4c3.7,0,7-1.8,9.1-4.6
+                        c2.3,4.2,6.2,6.3,6.4,6.4c0.9,0.5,2,0.2,2.6-0.6c2.6-3.7,3.9-8,3.9-12.6C54,21.9,44.1,12,32,12z M32,41.4c-4.1,0-7.4-3.3-7.4-7.4
+                        c0-4.1,3.3-7.4,7.4-7.4s7.4,3.3,7.4,7.4C39.4,38.1,36.1,41.4,32,41.4z"/>
+                    </g>
+                    <g>
+                      <path className="st3" d="M32,54c-12.1,0-22-9.9-22-22s9.9-22,22-22s22,9.9,22,22c0,4.5-1.4,8.9-3.9,12.6c-0.6,0.8-1.7,1.1-2.6,0.6
+                        c-0.3-0.2-8.1-4.2-8.1-13.1v-8.8c0-1.1,0.9-2,2-2c1.1,0,2,0.9,2,2v8.8c0,4.3,2.6,7.1,4.4,8.5C49.2,38,50,35,50,32
+                        c0-9.9-8.1-18-18-18s-18,8.1-18,18s8.1,18,18,18c2.9,0,5.7-0.7,8.2-2c1-0.5,2.2-0.1,2.7,0.9c0.5,1,0.1,2.2-0.9,2.7
+                        C39,53.2,35.5,54,32,54z"/>
+                    </g>
+                    <g>
+                      <path className="st3" d="M32,24.6c4.1,0,7.4,3.3,7.4,7.4s-3.3,7.4-7.4,7.4s-7.4-3.3-7.4-7.4S27.9,24.6,32,24.6 M32,20.6
+                        c-6.3,0-11.4,5.1-11.4,11.4S25.7,43.4,32,43.4S43.4,38.3,43.4,32S38.3,20.6,32,20.6L32,20.6z"/>
+                    </g>
+                  </g>
+                  <g id="Layer_2">
+                  </g>
+                  </svg>
                 </a>
             </li>
         </ul>
